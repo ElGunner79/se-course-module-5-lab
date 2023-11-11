4518a4447829203b72f6affb39ef378ce171e4b9
ElGunner79 <richard.brazao@outlook.com>
2023-11-11 15:42:54 +0800

    finished lab 7.1

--- m5-lab-3-6-7.1/index.js
+++ m5-lab-3-6-7.1/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const testRoutes = require('./routes/myTestRoutes');
 const calculatorRoutes = require('./routes/calculatorRoutes');
+//Lab 7.1
+const swaggerUi = require('swagger-ui-express');
+swaggerDocument = require('./swagger.json');
 const app = express()
 const port = 3000
 
@@ -9,6 +12,13 @@ app.use('/', express.static('public'))
 app.use('/mytest', testRoutes);
 
 app.use('/calculator', calculatorRoutes)
+
+//Lab 7.1
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+    );
 
 app.listen(port, () => {
     console.log(`Example app listening
