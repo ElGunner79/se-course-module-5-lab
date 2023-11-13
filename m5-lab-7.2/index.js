7ad746ecad18503709d5cac7e5f5d53da75f6502
ElGunner79 <richard.brazao@outlook.com>
2023-11-13 22:53:19 +0800

    small cosmetic changes to the code

--- m5-lab-7.2/index.js
+++ m5-lab-7.2/index.js
@@ -1,15 +1,15 @@
-// Dependencies
+// Dependencies.
 const express = require('express');
 const axios = require('axios');
 
-// Swagger Dependency
+// Swagger Dependency.
 const swaggerUi = require('swagger-ui-express');
 swaggerDocument = require('./swagger.json');
 
-// Initialize Express App
+// Initialize Express App.
 const app = express();
 
-// Set up a static server to serve files from the 'public' directory
+// Set up a static server to serve files from the 'public' directory.
 app.use(express.static('public'));
 
 // Allow requests from any origin
@@ -22,10 +22,10 @@ app.use((req, res, next) => {
 // Route to handle GET requests to '/products'
 app.get('/products', async (req, res) => {
     try {
-        // Fetch product data from the fake store API using axios
+        // Fetch product data from the fake store API using axios.
         const response = await axios.get('https://fakestoreapi.com/products');
 
-        // Return the fetched product data as the response
+        // Return the fetched product data as the response.
         res.json(response.data);
     } catch (error) {
         console.error(error);
